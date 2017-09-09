ab8ff5fd9c1bbe72f963bcf5d00e3a39a4b5bc55
Eloev Georgy <factl38@gmail.com>
2017-09-09 19:21:09 +0300

    fir

---
+++ порбный.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "stdio.h"
+
+int main()
+{
+	printf("Hello!!");
+	getchar();
+    return 0;
+}
+

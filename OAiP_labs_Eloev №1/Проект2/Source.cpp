6e249a8f4b312809c2355eae10e3bda01b49c4be
Eloev Georgy <factl38@gmail.com>
2017-09-30 14:57:30 +0300

    Lab

---
+++ OAiP_labs_Eloev №1/Проект2/Source.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGSNO
+#include <stdio.h>
+
+
+int main()
+{
+	system("chcp 1251");
+	system("cls");
+	float a;
+	printf("Введите количество узлов: ");
+	int x;
+	do {
+		x = scanf_s("%f", &a);
+		if (!x) {
+			printf("Ошибка ввода. Повторите попытку ввода: ");
+			while (getchar() != '\n');
+		}
+		else
+		{
+			float c = a *1.85;
+			float b = (c * 1000) / 3600;
+			printf("%.1f км/ч \n", c);
+			printf("%.1f м/с \n", b);
+		}
+	} while (x != 1);
+	_getch();
+	return 0;
+}
--- Тема №1 лабор. №1 Вариант №2/ConsoleApplication1/ConsoleApplication1.cpp
+++ Тема №1 лабор. №1 Вариант №2/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,18 +1,30 @@
 #include "stdafx.h"
-#include "iostream"
 #include <stdio.h>
-using namespace std;
 
 
 int main()
 {
 	float a;
-	printf ( "Enter the number of nodes: ") ; 
-	scanf_s("%f", &a);
-	float c = a *1.85;
-	float b = (c * 1000) / 3600;
-	printf("%.1f km/h \n", c);
-	printf("%.1f m/s \n", b);
-	system("pause");
+	printf ( "Enter the number of nodes: ") ;
+	int x = 0;
+	while (x==0)
+	{
+		scanf_s("%f", &a);
+		int r = scanf_s("%f", &a);
+		if (!r)
+		{
+			printf("Error!");
+			while (getchar() != "\n");
+		}
+		else
+		{
+			float c = a *1.85;
+			float b = (c * 1000) / 3600;
+			printf("%.1f km/h \n", c);
+			printf("%.1f m/s \n", b);
+			x = 1;
+		}
+	}
+	getch();
 	return 0;
 }

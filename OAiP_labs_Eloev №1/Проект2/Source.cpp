be23f6d6560837c909b18d0c060b9dfc300add56
Eloev Georgy <factl38@gmail.com>
2017-10-02 21:35:45 +0300

    New

--- OAiP_labs_Eloev №1/Проект2/Source.cpp
+++ OAiP_labs_Eloev №1/Проект2/Source.cpp
@@ -1,10 +1,11 @@
 /*Елоев Георгий Александрович
 КТБО 1-8
 Лабораторная № 1
-Вариант №2*/
-#define _CRT_SECURE_NO_WARNINGSNO
+Вариант №2
+Задание: Перевести узлы в км/ч и м/с*/
 #include <stdio.h>
-
+#define N 1.85 
+#define C 1000/3600
 
 int main()
 {
@@ -18,15 +19,14 @@ int main()
 		x = scanf_s("%f", &a);
 		if (!x)
 		{
-			printf("Ошибка ввода. Повторите попытку ввода: ");
+			printf("Ошибка ввода!!!!!!\nПовторите попытку ввода: ");
 			while (getchar() != '\n');
 		}
 		else
 		{
-			float c = a *1.85;
-			float b = (c * 1000) / 3600;
-			printf("%.1f км/ч \n", c);
-			printf("%.1f м/с \n", b);
+			float c = a *N;
+			float b = c * C;
+			printf("%.1f узлов = %.1f км/ч = %.1f м/с  \n",a,c,b); 
 		}
 	} while (x != 1);
 	_getch();
